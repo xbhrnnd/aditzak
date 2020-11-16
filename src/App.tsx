3cc2131107175a635c24357f2091f4aa99be13fc
Xabi Hernando <xabihernando@me.com>
2020-11-16 17:10:58 +0100

    lehenHitza removed

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { HashRouter, Switch, Route } from "react-router-dom";
 import { GoiburuaPod } from "./components/pods/goiburua.pod";
 import { HasieraScene } from "./components/scenes/hasiera.scene";
 import { AditzakScene } from "./components/scenes/aditzak.scene";
-import { LehenHitzaScene } from "./components/scenes/lehen-hitza.scene";
 import { HoniBuruzScene } from "./components/scenes/honiburuz.scene";
 import { OinaScene } from "./components/scenes/oina.scene";
 
@@ -16,7 +15,6 @@ function App() {
           <GoiburuaPod />
           <Route path="/" component={HasieraScene} exact></Route>
           <Route path="/aditzak" component={AditzakScene} exact></Route>
-          <Route path="/lehen-hitza" component={LehenHitzaScene} exact></Route>
           <Route path="/honiburuz" component={HoniBuruzScene} exact></Route>
           <OinaScene />
         </Fragment>
